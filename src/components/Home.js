31ae589cef2343ecc7fb746f742764d4a4d4ccc1
Eric Scaglioni <ericsf34@gmail.com>
2019-03-22 14:55:05 -0300

    Mudanças no css e novo componente

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Sidebar from './Sidebar';
 
 class Home extends Component {
     render() {
         return (
-            <h1>This is my Resumé!</h1>
+            <div>
+                <Sidebar />
+                <h1>This is my Resumé!</h1>
+            </div>
         );
     }
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+    render() {
+        return (
+            <h1>essa é a sidebar</h1>
+        );
+    }
+}
+
+export default SideBar;
